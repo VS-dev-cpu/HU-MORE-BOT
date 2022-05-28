4a4560b65359dd0094de4e0edfd6ce49b186cdcb
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-05-28 16:06:54 +0200

    Update start.py

--- source/start.py
+++ source/start.py
@@ -1,7 +1,7 @@
 import os
 import time
 import bt
-from gpiozero import button
+from gpiozero import Button
 
 def send(data):
     os.system("echo '" + str(data) + "\\n' >> /dev/ttyS0")
@@ -9,13 +9,14 @@ def send(data):
 bt = bt.BT()
 
 b = Button(26)
-
 while (1):
         if not b.is_pressed:
                 break
 
-#time.sleep(0)
-
+print("Starting...")
 bt.start()
 
+time.sleep(8)
+
+print("Done")
 send(1)
