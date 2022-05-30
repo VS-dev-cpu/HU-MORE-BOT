d8bf43869827e2a82e4a34204821c59ac8ec82a5
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-05-30 13:24:06 +0200

    Update and rename start.py to rooster.py

--- source/rooster.py
+++ source/rooster.py
@@ -1,17 +1,12 @@
 import os
 import time
 import bt
-from gpiozero import Button
 
 bt = bt.BT()
 
 def send(data):
     os.system("echo '" + str(data) + "\\n' >> /dev/ttyS0")
-
-b = Button(26)
-while (b.is_pressed):
-    pass
-
-bt.start()
+    
+bt.sync()
 time.sleep(8)
 send(1)
