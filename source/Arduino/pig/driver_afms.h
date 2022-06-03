8e5f874f82b5857758c12cd1da2b005e887bd6c8
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-06-03 16:46:22 +0100

    Create driver_afms.h

---
+++ source/Arduino/pig/driver_afms.h
@@ -0,0 +1,45 @@
+#include <Adafruit_MotorShield.h>
+
+Adafruit_DCMotor *m_left = AFMS.getMotor(2);
+Adafruit_DCMotor *m_right = AFMS.getMotor(1);
+
+int ms = 100;
+
+void prepare(int max_speed)
+{
+  ms = max_speed;
+  AFMS.begin();
+}
+
+void run(int left, int right)
+{
+  if (left > 0)
+  {
+    m_left->setSpeed(map(left, 0, 100, 0, ms));
+    m_left->run(FORWARD);
+  }
+  else if (left < 0)
+  {
+    m_left->setSpeed(map(-left, 0, 100, 0, ms));
+    m_left->run(BACKWARD);
+  }
+  else
+  {
+    m_left->run(RELEASE);
+  }
+
+  if (right > 0)
+  {
+    m_right->setSpeed(map(right, 0, 100, 0, ms));
+    m_right->run(FORWARD);
+  }
+  else if (right < 0)
+  {
+    m_right->setSpeed(map(-right, 0, 100, 0, ms));
+    m_right->run(BACKWARD);
+  }
+  else
+  {
+    m_right->run(RELEASE);
+  }
+}
