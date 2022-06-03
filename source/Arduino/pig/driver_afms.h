c05546cc8125ebde2813bea5cb32a79107d8d472
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-06-03 17:03:24 +0100

    Update driver_afms.h

--- source/Arduino/pig/driver_afms.h
+++ source/Arduino/pig/driver_afms.h
@@ -1,11 +1,13 @@
 #include <Adafruit_MotorShield.h>
 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
 Adafruit_DCMotor *m_left = AFMS.getMotor(2);
 Adafruit_DCMotor *m_right = AFMS.getMotor(1);
 
-int ms = 100;
+int max_speed = 100;
 
-void init()
+void driver_setup()
 {
   AFMS.begin();
 }
@@ -14,12 +16,12 @@ void run(int left, int right)
 {
   if (left > 0)
   {
-    m_left->setSpeed(map(left, 0, 100, 0, ms));
+    m_left->setSpeed(map(left, 0, 100, 0, max_speed));
     m_left->run(FORWARD);
   }
   else if (left < 0)
   {
-    m_left->setSpeed(map(-left, 0, 100, 0, ms));
+    m_left->setSpeed(map(-left, 0, 100, 0, max_speed));
     m_left->run(BACKWARD);
   }
   else
@@ -29,12 +31,12 @@ void run(int left, int right)
 
   if (right > 0)
   {
-    m_right->setSpeed(map(right, 0, 100, 0, ms));
+    m_right->setSpeed(map(right, 0, 100, 0, max_speed));
     m_right->run(FORWARD);
   }
   else if (right < 0)
   {
-    m_right->setSpeed(map(-right, 0, 100, 0, ms));
+    m_right->setSpeed(map(-right, 0, 100, 0, max_speed));
     m_right->run(BACKWARD);
   }
   else
